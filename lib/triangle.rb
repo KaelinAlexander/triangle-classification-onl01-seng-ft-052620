acbf13f0d80b96369ef82e0dbb0565b2749ca18c
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-11 17:25:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,13 @@
 class Triangle
-attr_accessor :len1, :len2, :len3
+attr_accessor :len1, :len2, :len3, :type
 
-def initialize(len1, len2, len3)
-  @len1 = len1
-  @len2 = len2
-  @len3 = len3
-end
+  def initialize(len1, len2, len3)
+    @len1 = len1
+    @len2 = len2
+    @len3 = len3
+  end
+
+  def kind
+    
+
+end 
